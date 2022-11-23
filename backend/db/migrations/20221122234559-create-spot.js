d51007cfa3289d5deaf38961659612b96ad57f56
andrea-green <108085381+andrea-green@users.noreply.github.com>
2022-11-23 09:57:49 -0700

    added Spots seeders

--- backend/db/migrations/20221122234559-create-spot.js
+++ backend/db/migrations/20221122234559-create-spot.js
@@ -50,7 +50,7 @@ module.exports = {
         allowNull:false,
       },
       price: {
-        type: Sequelize.DECIMAL,
+        type: Sequelize.INTEGER,
         allowNull:false,
       },
       createdAt: {
--- backend/db/seeders/20221123041144-spots.js
+++ backend/db/seeders/20221123041144-spots.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = 'Spots';
-    return queryInterface.bulkinsert(options, [
+    return queryInterface.bulkInsert(options, [
       {
         ownerId:1,
         address:'123 Avenue',
@@ -20,7 +20,7 @@ module.exports = {
         lng:-75.036468,
         name:'The Bushkill House',
         description:'The Bushkill House is a mountain escape located in the heart of the Poconos. On a wooded lot by a creek, the cabin offers spectacular views of the landscape. As designers for our studio, Ridge House (Insta @_ridgehouse), we were sure to curate the interiors.',
-        price:130,
+        price:130
       },
       {
         ownerId:2,
@@ -31,8 +31,8 @@ module.exports = {
         lat:39.750305,
         lng:-104.984630,
         name:'Colter Cottage',
-        description:'Welcome to the Colter Cottage - urban cottage meets European charm. Come stay in our beautiful carriage house in the heart of downtown Denver. '
-        price:150,
+        description:'Welcome to the Colter Cottage - urban cottage meets European charm. Come stay in our beautiful carriage house in the heart of downtown Denver. ',
+        price:150
       },
       {
         ownerId:3,
@@ -43,11 +43,10 @@ module.exports = {
         lat:41.119569,
         lng:-75.055324,
         name:'Ridge House',
-        description: 'The Ridge House is a mountain escape located in the heart of the Poconos. On a wooded lot perched on a mountain ridge, the cabin offers spectacular views of the beautiful landscape.'
-        price:143,
-      },
+        description: 'The Ridge House is a mountain escape located in the heart of the Poconos. On a wooded lot perched on a mountain ridge, the cabin offers spectacular views of the beautiful landscape.',
+        price:143
+      }
     ],{});
-
   },
 
   async down (queryInterface, Sequelize) {
