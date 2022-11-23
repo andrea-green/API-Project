5e5fd10190da63bde1cad3d30162b3e27c102302
andrea-green <108085381+andrea-green@users.noreply.github.com>
2022-11-23 10:30:42 -0700

    created Reviews seeder

---
+++ backend/db/seeders/20221123172021-Reviews.js
@@ -0,0 +1,42 @@
+'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = 'Reviews';
+    return queryInterface.bulkInsert(options,[
+      {
+        spotId:1,
+        userId:1,
+        review:"Cozy and warm!",
+        stars:5
+      },
+      {
+        spotId:2,
+        userId:2,
+        review:"We had an amazing time. The place is beautiful and a great location.",
+        stars:5
+      },
+      {
+        spotId:3,
+        userId:3,
+        review:"Great place to stay when exploring Bushkill Falls!",
+        stars:5
+      }
+    ])
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Reviews';
+    const Op = sequelize.Op;
+   return queryInterface.bulkDelete(options,{
+    id: { [Op.in]: [1,2,3]}
+   },{});
+
+  }
+};
