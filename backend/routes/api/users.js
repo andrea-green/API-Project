9b5dae3fb488beb224bfa3ede3b25d83f239eb56
andrea-green <108085381+andrea-green@users.noreply.github.com>
2022-11-28 20:25:50 -0700

    created username and email validation on user router

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,32 +8,58 @@ const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 const validateSignup = [
-  check('email')
-    .exists({ checkFalsy: true })
-    .isEmail()
-    .withMessage('Please provide a valid email.'),
+
   check('username')
-    .exists({ checkFalsy: true })
-    .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
+  .exists({ checkFalsy: true })
+  .isLength({ min: 4 })
+  .withMessage('Please provide a username with at least 4 characters.'),
   check('username')
-    .not()
-    .isEmail()
-    .withMessage('Username cannot be an email.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
-  handleValidationErrors
+  .not()
+  .isEmail()
+  .withMessage('Username cannot be an email.'),
+check('email')
+.exists({ checkFalsy: true })
+.isEmail()
+.withMessage('Please provide a valid email.'),
+check('password')
+.exists({ checkFalsy: true })
+.isLength({ min: 6 })
+.withMessage('Password must be 6 characters or more.'),
+handleValidationErrors
 ];
 
 // Sign up
 router.post(
-    '/',
-    validateSignup,
-    async (req, res) => {
-      const { email,firstName,lastName, password, username } = req.body;
+  '/',
+  validateSignup,
+  async (req, res) => {
+    const { email,firstName,lastName, password, username } = req.body;
+
+    const myUserName = await User.findOne({where:{username}})
+    if(myUserName){
+      res.status(403);
+      return res.json({
+        "message": "User already exists",
+        "statusCode": 403,
+        "errors": {
+          "username": "User with that username already exists"
+        }
+      })
+    }
+
+    const myEmail = await User.findOne({where:{email}})
+    if(myEmail){
+      res.status(403);
+      return res.json({
+        "message": "User already exists",
+        "statusCode": 403,
+        "errors": {
+          "email": "User with that email already exists"
+        }
+      })
+    }
       const user = await User.signup({ email,firstName,lastName, username, password });
+
 
       await setTokenCookie(res, user);
 
