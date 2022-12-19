49e12d29b12a0d75620526ab46e73c87ed067e92
andrea-green <108085381+andrea-green@users.noreply.github.com>
2022-12-19 04:01:14 -0700

    dropdown menu css done

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -46,27 +46,29 @@ function ProfileButton({ user }) {
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
   return (
-    <div>
+    <div className='dropdown-menu-div'>
       <button className='button' onClick={openMenu}>
         <i class="fa-solid fa-user"></i>
       </button>
+
       <ul className={ulClassName} ref={ulRef}>
         {user ? (
           <>
             <div className='user-info-div'>
-              <li>{user.username}</li>
-              <li>{user.firstName} {user.lastName}</li>
-              <li>{user.email}</li>
+              <li className='form-input'>{user.username}</li>
+              <li className='form-input'>{user.firstName} {user.lastName}</li>
+              <li className='form-input'>{user.email}</li>
             </div>
-            <div className='button'>
+            <div className='button form-input'>
 
               <OpenModalMenuItem
+
                 itemText="List Your Property"
                 onItemClick={closeMenu}
                 modalComponent={<CreateSpotForm />}
               />
             </div>
-            <div className="button">
+            <div className="button form-input">
               <OpenModalMenuItem
                 itemText="My Reviews"
                 onItemClick={closeMenu}
@@ -74,7 +76,7 @@ function ProfileButton({ user }) {
               />
             </div>
             <li>
-              <button className='button' onClick={logout}>Log Out</button>
+              <button className='button form-button' onClick={logout}>Log Out</button>
             </li>
           </>
         ) : (
