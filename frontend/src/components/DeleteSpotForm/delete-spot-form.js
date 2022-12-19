6e8eff1d2e474df91e7f72fb39c3fdf3b3cae0ea
andrea-green <108085381+andrea-green@users.noreply.github.com>
2022-12-19 01:49:00 -0700

    delete spot form css done

--- frontend/src/components/DeleteSpotForm/delete-spot-form.js
+++ frontend/src/components/DeleteSpotForm/delete-spot-form.js
@@ -38,39 +38,41 @@ const DeleteSpotForm = () => {
 
     return (
         <div>
-            <h1>Are you sure you want to delete?</h1>
-            <section className='delete-spot-form'>
-                    <div className='delete-errors'>
-                        <ul>{errorValidations.map((error) => (
-                            <li key={error}>{error}</li>
-                        ))}
-                        </ul>
-                    </div>
-                    <form onSubmit={handleSubmit}>
-                        <label>
-                            No
-                            <input
-                                type="radio"
-                                required
-                                checked={boolean ? false : true }
-                                onChange={falseBoolean}
-                            />
-                        </label>
-                        <label>
-                            Yes
-                            <input
-                                type="radio"
-                                required
-                                checked={boolean}
-                                onChange={trueBoolean}
-                            />
-                        </label>
-                        <button
-                        className='button'
+            <div className='form-header'>
+                <h1>Are you sure you want to delete?</h1>
+            </div>
+            <section className='form-body-container'>
+                <div className='delete-errors'>
+                    <ul>{errorValidations.map((error) => (
+                        <li key={error}>{error}</li>
+                    ))}
+                    </ul>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        No
+                        <input className='button'
+                            type="radio"
+                            required
+                            checked={boolean ? false : true}
+                            onChange={falseBoolean}
+                        />
+                    </label>
+                    <label>
+                        Yes
+                        <input className='button'
+                            type="radio"
+                            required
+                            checked={boolean}
+                            onChange={trueBoolean}
+                        />
+                    </label>
+                    <button
+                        className='button form-button'
                         type="submit"
                         disabled={!boolean}
-                        >Confirm</button>
-                    </form>
+                    >Confirm</button>
+                </form>
             </section>
 
 
