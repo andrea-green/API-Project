3999d5a3394a98d327801764d2651f799a966ef1
andrea-green <108085381+andrea-green@users.noreply.github.com>
2022-12-18 17:11:16 -0700

    single spots details added

--- frontend/src/css-modules/singleSpot/singleSpot.js
+++ frontend/src/css-modules/singleSpot/singleSpot.js
@@ -1,15 +1,18 @@
 import React from "react";
 
+
 import { useSelector } from "react-redux";
 
- function SingleSpotDetails() {
+function SingleSpotDetails() {
     // const { city,state,avgRating,price,previewImage } = useParams()
     // need to deconstruct so that i can just reference those things.
 
-    const spot = useSelector((state)=>state.Spots.singleSpot);
+    const spot = useSelector((state) => state.Spots.singleSpot);
 
     // const spotPic = useSelector((state)=>state.SpotImages.url)
-    const spotPic = spot.SpotImages.find(imgObj => imgObj.preview===true)
+    const spotPic = spot.SpotImages.find(imgObj => imgObj.preview === true)
+    const owner = useSelector((state) => state.Spots.singleSpot.Owner.firstName)
+
 
     return (
         <div>
@@ -18,7 +21,9 @@ import { useSelector } from "react-redux";
             </div>
 
             <div className='city-state-review-div'>
-                <div>{spot.address}</div>
+                <i class="fa-regular fa-star">{spot.avgStarRating}</i>
+                <div>{spot.numReview}</div>
+                <i class="fa-solid fa-trophy">Superhost</i>
                 <div>{spot.city}</div>
                 <div>{spot.state}</div>
                 <div>{spot.country}</div>
@@ -26,6 +31,31 @@ import { useSelector } from "react-redux";
 
             <div className='spot-preview-image-div'>
                 <img src={spotPic.url} alt='spot-pic-url'></img>
+            </div>
+
+            <div className='share-and-like'>
+                <i class="fa-solid fa-arrow-up-from-bracket">share</i>
+                <i class="fa-solid fa-heart">Like</i>
+
+            </div>
+            <div className='hosted-by-header'>
+                <h1>Entire rental unit hosted by {owner} </h1>
+            </div>
+            <div className='first-details-section'>
+                <div>
+                    <i class="fa-regular fa-user">Self check-in</i>
+                    <div> Check yourself in with the smartlock</div>
+                </div>
+                <div>
+                <i class="fa-solid fa-trophy">{owner} is a Superhost</i>
+                <div>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</div>
+                <i class="fa-solid fa-key">Great check-in experience</i>
+                <div>92% of recent guests gave the check-in process a 5-star rating. </div>
+                </div>
+            </div>
+            <div className='snow-cover-div'>
+                <h1>Snow Cover</h1>
+                <div>Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.</div>
             </div>
         </div>
     )
