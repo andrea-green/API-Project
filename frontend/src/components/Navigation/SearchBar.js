36ca76aefa3e62369585722ee5e621bb302bfcc1
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-03-19 14:43:34 -0500

    search bar functionality

--- frontend/src/components/Navigation/SearchBar.js
+++ frontend/src/components/Navigation/SearchBar.js
@@ -1,29 +1,36 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React , { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import TextField from "@mui/material/TextField";
 import List from './List';
 
 function SearchBar() {
-    const [inputText, setInputText] = useState("");
-    let inputHandler = (e) => {
-        //convert input text to lower case
-        var lowerCase = e.target.value.toLowerCase();
-        setInputText(lowerCase);
-    }
-    return (
-        <div>
-            <div className="search" style={{width:'30rem'}}>
-                <TextField
-                    id="outlined-basic"
-                    onChange={inputHandler}
-                    variant="outlined"
-                    fullWidth
-                    label="Search for a city"
-                />
-            </div>
-            <List input={inputText}/>
-        </div>
-    );
+  const [inputText, setInputText] = useState('');
+  const location = useLocation();
+
+  useEffect(() => {
+    setInputText('');
+  }, [location]);
+
+  const handleInput = (e) => {
+    const value = e.target.value.toLowerCase();
+    setInputText(value);
+  };
+
+  return (
+    <div>
+      <div className="search" style={{ width: '30rem' }}>
+        <TextField
+          id="outlined-basic"
+          onChange={handleInput}
+          value={inputText}
+          variant="outlined"
+          fullWidth
+          label="Search for a city"
+        />
+      </div>
+      <List input={inputText} />
+    </div>
+  );
 }
 
 export default SearchBar;
